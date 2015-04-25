13a43e6dc2a3632e6236efe5cf1135656393d596
Eric Guo <eric.guo@sandisk.com>
2015-04-25 23:12:24 +0800

    fix typo

--- coders/w_uncle.rb
+++ coders/w_uncle.rb
@@ -6,7 +6,7 @@ class WUncle < Coder
 
   def work(remain_difficulty)
     forward = rand(5...10)
-    puts randdom_events(forward)
+    puts random_events(forward)
     remain_difficulty - forward
   end
 
@@ -15,7 +15,7 @@ class WUncle < Coder
     company_money - salary
   end
 
-  def randdom_events(forward)
+  def random_events(forward)
     events = [
         "由于机房爆炸,#{name}被所有大佬抓出来艹翻了，导致项目进度下降#{forward}",
         "由于#{name}被四爷抓去打玩家,业务故障无人处理,导致项目进度下降#{forward}",
